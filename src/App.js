742179eb1c5a2d10fd5e7630574cc803015c7382
Lejla-dev <77573515+Lejla-dev@users.noreply.github.com>
2021-10-10 18:35:21 +0200

    added

---
+++ src/App.js
@@ -0,0 +1,41 @@
+
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="London" />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/nmrndll"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Lejla Latic
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/nmrndll/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://elastic-franklin-aa5196.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
